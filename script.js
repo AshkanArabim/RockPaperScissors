79f3b14894a1ea864762cd4ec2e4d1cbf5209664
Ashkan <ashkan.arabim@gmail.com>
2022-05-07 17:20:42 -0600

    assign choice to buttons

--- script.js
+++ script.js
@@ -21,6 +21,17 @@ function playerPlay() {
     "Please choose one of the following: Rock, Paper, Scissors (You are player 1)"
   ).toLowerCase();
 
+  //   let choice = "";
+  //   let choices = document.querySelectorAll("button.choice");
+
+  //   choices.forEach((button) =>
+  //     button.addEventListener("click", () => {
+  //       choice = button.id;
+  //     })
+  //   );
+
+  //   alert(choice);
+
   while (!["rock", "paper", "scissors"].includes(choice)) {
     choice = prompt(
       "That is not a valid choice. Please choose one of the following: Rock, Paper, Scissors (You are player 1)"
@@ -32,8 +43,8 @@ function playerPlay() {
 
 function playRound(player1, player2) {
   let winner;
-  console.log(`\tPlayer 1: ${player1}`);
-  console.log(`\tPlayer 2: ${player2}`);
+  console.log(`\tHuman: ${player1}`);
+  console.log(`\tComputer: ${player2}`);
   if (player1 === player2) {
     console.log("\ttie");
   } else {
@@ -42,44 +53,52 @@ function playRound(player1, player2) {
       (player1 === "paper" && player2 === "rock") ||
       (player1 === "scissors" && player2 === "paper")
     ) {
-      console.log("\tWinner: Player 1");
+      console.log("\tWinner: Human");
       winner = "p1";
     } else {
-      console.log("\tWinner: Player 2");
+      console.log("\tWinner: Computer");
       winner = "p2";
     }
   }
   return winner;
 }
 
-function game() {
-  let p1wins = 0;
-  let p2wins = 0;
+i = 0;
+let p1wins = 0;
+let p2wins = 0;
 
-  for (i = 1; i <= 5; ++i) {
-    console.log(`Round ${i} :`);
-    let winner = playRound(playerPlay(), computerPlay());
+function game(choice) {
+  console.log(`----------\nRound ${i} :`);
+  let winner = playRound(choice, computerPlay());
+  i++;
 
-    switch (winner) {
-      case "p1":
-        p1wins++;
-        break;
-      case "p2":
-        p2wins++;
-        break;
-      default:
-        break;
-    }
-
-    console.log(`Scores:\n\tPlayer 1: ${p1wins}\n\tPlayer 2: ${p2wins}`);
-    console.log("-----------");
+  switch (winner) {
+    case "p1":
+      p1wins++;
+      break;
+    case "p2":
+      p2wins++;
+      break;
+    default:
+      break;
   }
 
-  if (p1wins > p2wins) {
-    console.log("The final winner is Player 1!");
-  } else if (p1wins < p2wins) {
-    console.log("The final winner is Player 2!");
-  } else {
-    console.log("It's a tie!");
-  }
+  console.log(`Human: ${p1wins}\nComputer: ${p2wins}`);
+  //   if (p1wins > p2wins) {
+  //     console.log("The final winner is Player 1!");
+  //   } else if (p1wins < p2wins) {
+  //     console.log("The final winner is Player 2!");
+  //   } else {
+  //     console.log("It's a tie!");
+  //   }
 }
+
+let choice = "";
+let choices = document.querySelectorAll("button.choice");
+
+choices.forEach((button) =>
+  button.addEventListener("click", () => {
+    choice = button.id;
+    game(choice);
+  })
+);
